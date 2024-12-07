0b038d38e9b0e57e55103b257b3b409e26e94e29
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2024-12-07 13:50:45 -0700

    check 1.21.4 players in NoSlowC and NoSlowD

--- src/main/java/ac/grim/grimac/checks/impl/movement/NoSlowC.java
+++ src/main/java/ac/grim/grimac/checks/impl/movement/NoSlowC.java
@@ -5,6 +5,7 @@ import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PostPredictionCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.anticheat.update.PredictionComplete;
+import ac.grim.grimac.utils.enums.FluidTag;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 
 @CheckData(name = "NoSlowC", setback = 5, experimental = true)
@@ -17,19 +18,15 @@ public class NoSlowC extends Check implements PostPredictionCheck {
     public void onPredictionComplete(final PredictionComplete predictionComplete) {
         if (!predictionComplete.isChecked()) return;
 
-        if (player.isSlowMovement) {
+        if (player.isSlowMovement && player.sneakingSpeedMultiplier < 0.8f) {
             ClientVersion client = player.getClientVersion();
 
             // https://bugs.mojang.com/browse/MC-152728
-            if (client.isNewerThanOrEquals(ClientVersion.V_1_14_2)) {
+            if (client.isNewerThanOrEquals(ClientVersion.V_1_14_2) && client.isOlderThan(ClientVersion.V_1_21_4)) {
                 return;
             }
 
-            if (player.isSprinting
-                    // you can sneak and swim in 1.13 - 1.14.1
-                    && (!player.isSwimming || client.isNewerThan(ClientVersion.V_1_14_1) || client.isOlderThan(ClientVersion.V_1_13))
-                    && player.sneakingSpeedMultiplier < 0.8f
-            ) {
+            if (player.isSprinting && !player.isSwimming && (player.fluidOnEyes != FluidTag.WATER || client.isOlderThan(ClientVersion.V_1_21_4))) {
                 if (flagWithSetback()) alert("");
             } else reward();
         }
