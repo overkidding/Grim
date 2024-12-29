a4624f6907ad1c612452ac47e77fb4dc0c245454
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2024-12-28 19:36:26 -0700

    add BadPacketsV description

--- src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsV.java
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsV.java
@@ -10,7 +10,7 @@ import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 import com.github.retrooper.packetevents.util.Vector3d;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
 
-@CheckData(name = "BadPacketsV", experimental = true)
+@CheckData(name = "BadPacketsV", description = "Did not move far enough", experimental = true)
 public class BadPacketsV extends Check implements PacketCheck {
     public BadPacketsV(GrimPlayer player) {
         super(player);
