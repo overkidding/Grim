b6a79b74b6626d966b97c4008f9f433156dd95da
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-01-18 18:45:39 -0700

    this also applies to horses

--- src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsQ.java
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsQ.java
@@ -5,7 +5,6 @@ import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
-import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType.Play.Client;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientEntityAction;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientEntityAction.Action;
@@ -20,9 +19,8 @@ public class BadPacketsQ extends Check implements PacketCheck {
     public void onPacketReceive(PacketReceiveEvent event) {
         if (event.getPacketType() == Client.ENTITY_ACTION) {
             WrapperPlayClientEntityAction wrapper = new WrapperPlayClientEntityAction(event);
-            // Camels are able to send negative jump boost, how and why!?
-            if (wrapper.getJumpBoost() < 0 && (!player.inVehicle() || player.compensatedEntities.getSelf().getRiding().getType() != EntityTypes.CAMEL)
-                    || wrapper.getJumpBoost() > 100
+            // you are able to send negative jump boost, how and why!?
+            if (Math.abs(wrapper.getJumpBoost()) > 100
                     || wrapper.getEntityId() != player.entityID
                     || wrapper.getAction() != Action.START_JUMPING_WITH_HORSE && wrapper.getJumpBoost() != 0) {
                 if (flagAndAlert("boost=" + wrapper.getJumpBoost() + ", action=" + wrapper.getAction() + ", entity=" + wrapper.getEntityId()) && shouldModifyPackets()) {
