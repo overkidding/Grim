16c0d37b545346fa23c02843c6f8683da0fa93cb
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-02-21 17:30:16 -0700

    include block type in fastbreak verbose

--- src/main/java/ac/grim/grimac/checks/impl/breaking/FastBreak.java
+++ src/main/java/ac/grim/grimac/checks/impl/breaking/FastBreak.java
@@ -72,7 +72,7 @@ public class FastBreak extends Check implements BlockBreakCheck {
             }
 
             if (blockDelayBalance > 1000) { // If more than a second of advantage
-                if (flagAndAlert("delay=" + breakDelay) && shouldModifyPackets()) {
+                if (flagAndAlert("delay=" + breakDelay + ", type=" + blockBreak.block.getType()) && shouldModifyPackets()) {
                     blockBreak.cancel();
                 }
             }
@@ -94,7 +94,7 @@ public class FastBreak extends Check implements BlockBreakCheck {
             }
 
             if (blockBreakBalance > 1000) { // If more than a second of advantage
-                if (flagAndAlert("diff=" + diff + ", balance=" + blockBreakBalance) && shouldModifyPackets()) {
+                if (flagAndAlert("diff=" + diff + ", balance=" + blockBreakBalance + ", type=" + blockBreak.block.getType()) && shouldModifyPackets()) {
                     blockBreak.cancel();
                 }
             }
